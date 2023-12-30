46584ff71532236241922a1ce3cb29fb4a4d0d04
danielhuaman <danielsam_m@hotmail.com>
2023-12-30 05:21:25 -0500

    Nuevas optimizaciones

--- script.js
+++ script.js
@@ -3,30 +3,48 @@ document.addEventListener('DOMContentLoaded', function () {
     const player = document.querySelector('.player');
     const container = document.querySelector('.game-container');
     const backgroundMusic = document.getElementById('backgroundMusic');
-    const pauseModal = new bootstrap.Modal(document.getElementById('pauseModal')); // Crea una instancia del modal Bootstrap
-    let playerPosition = 50; // posición inicial del jugador en porcentaje
+    const pauseModal = new bootstrap.Modal(document.getElementById('pauseModal'));
+    const scoreDisplay = document.querySelector('.score');
+    const collisionSound = new Audio('Explocion.mp3');
+    let playerPosition = 50;
     let score = 0;
     let asteroidInterval;
     const asteroids = [];
 
-    // Inicia la reproducción del video al cargar la página
     video.play();
 
-    // Evento que se activa cuando el video alcanza el tiempo final
     video.addEventListener('timeupdate', function () {
-        // Puedes ajustar el valor según tus necesidades
         if (video.currentTime >= video.duration) {
-            video.currentTime = 0; // Reinicia la reproducción al principio
+            video.currentTime = 0;
             video.play();
         }
     });
 
-    function updatePlayerPosition() {
-        player.style.left = playerPosition + '%';
+    // Verificar si la imagen ya está en el Local Storage
+    if (!localStorage.getItem('asteroidImage')) {
+        // Si no está, la carga y la almacena
+        const img = new Image();
+        img.onload = function () {
+            localStorage.setItem('asteroidImage', getBase64Image(img));
+        };
+        img.src = 'asteroide.svg';
+    }
+    // Verificar si la imagen ya está en el Local Storage
+    if (!localStorage.getItem('asteroidImage2')) {
+        // Si no está, la carga y la almacena
+        const img = new Image();
+        img.onload = function () {
+            localStorage.setItem('asteroidImage2', getBase64Image(img));
+        };
+        img.src = 'Asteroidep.png';
     }
 
-    function handleKeyPress(event) {
-        const movementIncrement = 2; // Ajusta según tus preferencias de movimiento
+    const updatePlayerPosition = () => {
+        player.style.left = playerPosition + '%';
+    };
+
+    const handleKeyPress = (event) => {
+        const movementIncrement = 2;
 
         if (event.key === 'ArrowLeft' && playerPosition > 0) {
             playerPosition -= movementIncrement;
@@ -35,52 +53,49 @@ document.addEventListener('DOMContentLoaded', function () {
         } else if (event.key === 's' || event.key === 'S') {
             createBullet();
         } else if (event.key === 'p' || event.key === 'P') {
-            togglePause(); // Agregado: Maneja la tecla "P" para pausar/reanudar
+            togglePause();
         } else if (event.key === 'm' || event.key === 'M') {
-            toggleBackgroundMusic(); // Agregado: Maneja la tecla "m" para pausar la musica/reanudar la musica
+            toggleBackgroundMusic();
         }
 
         updatePlayerPosition();
-    }
+    };
 
-    function createBullet() {
+    const createBullet = () => {
         const bullet = document.createElement('div');
         bullet.className = 'bullet';
         bullet.style.left = playerPosition + '%';
-        bullet.style.bottom = '140px'; // Ajusta la altura inicial de la bala según tus necesidades
+        bullet.style.bottom = '140px';
         container.appendChild(bullet);
 
         moveBullet(bullet);
-    }
+    };
 
-    function moveBullet(bullet) {
+    const moveBullet = (bullet) => {
         const bulletSpeed = 5;
-        const bulletInterval = setInterval(() => {
+
+        const move = () => {
             const bulletPosition = parseFloat(bullet.style.bottom) || 0;
             bullet.style.bottom = bulletPosition + bulletSpeed + 'px';
 
             if (bulletPosition > container.clientHeight) {
-                clearInterval(bulletInterval);
                 if (container.contains(bullet)) {
                     container.removeChild(bullet);
                 }
             } else {
-                // Comprueba colisiones con enemigos
                 const enemies = document.querySelectorAll('.enemy');
                 checkCollisionWithEnemies(bullet, enemies);
+                requestAnimationFrame(move);
             }
-        }, 16); // Aproximadamente 60 FPS
-    }
+        };
 
+        requestAnimationFrame(move);
+    };
 
-
-    // Crear una única instancia de audio
-    const collisionSound = new Audio('Explocion.mp3');
-
-    function checkCollisionWithEnemies(bullet, enemies) {
+    const checkCollisionWithEnemies = (bullet, enemies) => {
         const bulletRect = bullet.getBoundingClientRect();
 
-        enemies.forEach((enemy) => {
+        for (const enemy of enemies) {
             const enemyRect = enemy.getBoundingClientRect();
 
             if (
@@ -89,49 +104,39 @@ document.addEventListener('DOMContentLoaded', function () {
                 bulletRect.right > enemyRect.left &&
                 bulletRect.left < enemyRect.right
             ) {
-                // Colisión con un enemigo, incrementa la puntuación y elimina la bala y el enemigo
-                if (container.contains(bullet)) {
-                    container.removeChild(bullet);
-                }
-
-                playCollisionSound(); // Agregado: Reproduce el sonido de colisión
+                container.removeChild(bullet);
+                playCollisionSound();
                 incrementScore();
-
-                // Cambia el fondo del enemigo al chocar
-                enemy.style.backgroundImage = "url('Asteroidep.png')";
-
-                // Espera 1 segundo antes de eliminar el enemigo
+                enemy.style.backgroundImage = `url('${localStorage.getItem('asteroidImage2')}')`;
                 setTimeout(() => {
                     if (container.contains(enemy)) {
                         container.removeChild(enemy);
                     }
-                }, 100); // 1000 milisegundos = 1 segundo
+                }, 100);
+                break;
             }
-        });
-    }
+        }
+    };
 
-
-
-    function playCollisionSound() {
-        // Reiniciar el sonido en caso de que aún se esté reproduciendo
+    const playCollisionSound = () => {
         collisionSound.currentTime = 0;
         collisionSound.play();
-    }
+    };
 
+    const enemyTemplate = document.createElement('div');
+    enemyTemplate.className = 'enemy';
+    enemyTemplate.style.backgroundImage = `url('${localStorage.getItem('asteroidImage')}')`;
 
-
-
-    function createEnemy() {
-        const enemy = document.createElement('div');
-        enemy.className = 'enemy';
+    const createEnemy = () => {
+        const enemy = enemyTemplate.cloneNode(true);
         enemy.style.left = Math.random() * (container.clientWidth - 30) + 'px';
         container.appendChild(enemy);
         asteroids.push(enemy);
 
         moveEnemy(enemy);
-    }
+    };
 
-    function moveEnemy(enemy) {
+    const moveEnemy = (enemy) => {
         enemy.interval = setInterval(() => {
             const enemyPosition = parseFloat(enemy.style.top) || 0;
             enemy.style.top = enemyPosition + 3 + 'px';
@@ -146,94 +151,89 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 }
             }
-        }, 16); // Aproximadamente 60 FPS
-    }
+        }, 16);
+    };
 
-    function incrementScore() {
-        score += 10; // Puedes ajustar la cantidad de puntos según tu preferencia
+    const incrementScore = () => {
+        score += 10;
         updateScoreDisplay();
         animateScoreIncrease();
-    }
+    };
 
-    function animateScoreIncrease() {
-        const scoreDisplay = document.querySelector('.score');
+    const animateScoreIncrease = () => {
         scoreDisplay.classList.add('increase');
 
-        // Después de un breve tiempo, quita la clase de aumento para volver al tamaño normal
         setTimeout(() => {
             scoreDisplay.classList.remove('increase');
-        }, 200); // Ajusta la duración de la animación aquí (en milisegundos)
-    }
+        }, 200);
+    };
 
-    function updateScoreDisplay() {
-        // Actualiza el marcador de puntuación en la interfaz
-        const scoreDisplay = document.querySelector('.score');
+    const updateScoreDisplay = () => {
         scoreDisplay.textContent = `Score: ${score}`;
-    }
+    };
 
-    function togglePause() {
+    const togglePause = () => {
         if (video.paused) {
-            // Si el video está pausado, reanuda el juego
             video.play();
             startGame();
             asteroids.forEach((asteroid) => moveEnemy(asteroid));
-            // Oculta el modal de pausa
             pauseModal.hide();
         } else {
-            // Si el video está reproduciéndose, pausa el juego
             video.pause();
             clearInterval(asteroidInterval);
             asteroids.forEach((asteroid) => clearInterval(asteroid.interval));
-            // Muestra el modal de pausa
             pauseModal.show();
         }
-    }
+    };
 
-    function startGame() {
-        // Limpia el intervalo existente antes de comenzar uno nuevo
+    const startGame = () => {
         clearInterval(asteroidInterval);
         asteroidInterval = setInterval(() => {
             createEnemy();
-        }, 2000); // Genera un enemigo cada 2000 milisegundos (2 segundos)
-    }
+        }, 2000);
+    };
 
     startGame();
 
     document.addEventListener('keydown', handleKeyPress);
 
-    // Evento que se activa cuando la pestaña pierde el foco
-    window.addEventListener('blur', function () {
-        // Pausa la reproducción del video y cualquier otra actividad del juego
+    window.addEventListener('blur', () => {
         video.pause();
-        clearInterval(asteroidInterval); // Pausa la generación de asteroides
-        asteroids.forEach((asteroid) => clearInterval(asteroid.interval)); // Pausa el movimiento de asteroides existentes
-        // Implementa lógica adicional de pausa aquí...
+        clearInterval(asteroidInterval);
+        asteroids.forEach((asteroid) => clearInterval(asteroid.interval));
         pauseModal.show();
     });
 
-    // Evento que se activa cuando la pestaña obtiene el foco
-    window.addEventListener('focus', function () {
-        // Reanuda la reproducción del video y cualquier otra actividad del juego
+    window.addEventListener('focus', () => {
         video.play();
-        startGame(); // Reanuda la generación de asteroides
-        asteroids.forEach((asteroid) => moveEnemy(asteroid)); // Reanuda el movimiento de asteroides existentes
-        // Implementa lógica adicional de reanudación aquí...
+        startGame();
+        asteroids.forEach((asteroid) => moveEnemy(asteroid));
         pauseModal.hide();
     });
-    // Event listener para la tecla F5 (código de tecla 116) usando ASCII
-    document.addEventListener('keydown', function (event) {
+
+    document.addEventListener('keydown', (event) => {
         if (event.keyCode === 116) {
-            // Redirige a la página "index.html" al presionar F5
             window.location.href = 'index.html';
         }
     });
-    function toggleBackgroundMusic() {
-        const backgroundMusic = document.getElementById('backgroundMusic');
+
+    const toggleBackgroundMusic = () => {
         if (backgroundMusic.paused) {
             backgroundMusic.play();
         } else {
             backgroundMusic.pause();
         }
-    }
+    };
 
+    function getBase64Image(img) {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+
+        const dataURL = canvas.toDataURL('image/png');
+        return dataURL;
+    }
 });
